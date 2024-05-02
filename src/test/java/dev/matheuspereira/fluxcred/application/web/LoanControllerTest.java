9bfe7421109679c887ce127244b7a65fee42d473
Matheus C. Pereira <mcostap80@gmail.com>
2024-05-02 01:31:11 -0300

    Finalizando desenvolvimento de testes unitários

---
+++ src/test/java/dev/matheuspereira/fluxcred/application/web/LoanControllerTest.java
@@ -0,0 +1,6 @@
+package dev.matheuspereira.fluxcred.application.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LoanControllerTest {
+  
+}
---
+++ src/test/java/dev/matheuspereira/fluxcred/domain/handler/ManualApprovalHandlerTest.java
@@ -0,0 +1,6 @@
+package dev.matheuspereira.fluxcred.domain.handler;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ManualApprovalHandlerTest {
+  
+}
